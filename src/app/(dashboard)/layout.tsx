6f3eaa0620c7ac98a2a4e81ae7b10281d251c226
Jeff Jiang <jeff.jiang13@gmail.com>
2024-06-05 19:18:24 -0400

    first commit

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -8,19 +8,43 @@ type Props = {
 }
 
 const OwnerLayout = async ({ children }: Props) => {
-  const authenticated = await onLoginUser()
-  if (!authenticated) return null
+  const response = await onLoginUser()
 
-  return (
-    <ChatProvider>
-      <div className="flex h-screen w-full">
-        <SideBar domains={authenticated.domain} />
-        <div className="w-full h-screen flex flex-col pl-[4rem] md:pl-4">
-          {children}
+  // Check if the response has a user and domain property
+  if ('user' in response && 'domain' in response) {
+    const authenticated = response as {
+      user: {
+        id: string
+        fullname: string
+        type: string
+      }
+      domain: {
+        customer: {
+          chatRoom: {
+            id: string
+            live: boolean
+          }[]
+        }[]
+        id: string
+        name: string
+        icon: string
+      }[] | undefined
+    }
+
+    return (
+      <ChatProvider>
+        <div className="flex h-screen w-full">
+          <SideBar domains={authenticated.domain} />
+          <div className="w-full h-screen flex flex-col pl-[4rem] md:pl-4">
+            {children}
+          </div>
         </div>
-      </div>
-    </ChatProvider>
-  )
+      </ChatProvider>
+    )
+  }
+
+  // Return null or an appropriate fallback if the user is not authenticated
+  return null
 }
 
 export default OwnerLayout
